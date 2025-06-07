67d29eeac0ccf41c92031eff2b23ad6fa581f9ea
kanta1108 <67371445+kanta1108@users.noreply.github.com>
2025-06-07 22:29:52 +0900

    Set favorites API to node runtime

--- app/api/favorites/route.ts
+++ app/api/favorites/route.ts
@@ -2,7 +2,7 @@
 import app from '@/server/hono'
 import { handle } from 'hono/vercel'
 
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 export const GET = handle(app)
 export const POST = handle(app)
 export const DELETE = handle(app)
